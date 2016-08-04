5601227ebde4f4a7622b84f0c1fb618d8676e0e7
Elizabeth Alexander <eealexander2@gmail.com>
2016-08-04 16:06:10 -0700

    Created by Coding Ground Client

---
+++ New Project-20160804/com/models/Drink.java
@@ -0,0 +1,29 @@
+package com.models; 
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Drink {
+    private int price;
+    private Map<Ingredient, Integer> recipe;
+    private int menuPosition;
+
+    public Drink(Map<Ingredient, Integer> recipe, int menuPosition) {
+        this.recipe = new HashMap<>(recipe);
+        this.menuPosition = menuPosition;
+        this.price = setPrice();
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    private int setPrice() {
+        int total = 0;
+        for (Map.Entry<Ingredient, Integer> item : recipe.entrySet() ) {
+            int value = item.getKey().getPrice() * item.getValue();
+            total += value;
+        }
+        return total;
+    }
+}
